a1f7fc4949bb0626b29dd442261c4a1ad63505e2
Jingxiang Zou <njumath@sina.cn>
2020-06-23 08:50:46 +0000

    Upload New File

---
+++ src/fch_mo_copy.f90
@@ -0,0 +1,92 @@
+! written by jxzou at 20180208
+! updated by jxzou at 20200402: renamed from fchk_mo_copy to fch_mo_copy
+! updated by jxzou at 20200622: simplify code, add support input idx1, idx2, and combined with rwwfn.f90
+
+! This is a program/subroutine used to copy MOs from one .fchk file into another .fchk file
+
+program main
+ implicit none
+ integer :: i, idx1, idx2
+ integer, parameter :: iout = 6
+ character(len=3) :: ab
+ character(len=8) :: str
+ character(len=240) :: fchname1, fchname2
+
+ idx1 = 0; idx2 = 0
+ fchname1 = ' '; fchname2 = ' '
+ ab = '-aa'
+
+ i = iargc()
+ if(i<2 .or. i>4) then
+  write(iout,'(/,A)') ' ERROR in subroutine fch_mo_copy: wrong command line arguments!'
+  write(iout,'(A)')   ' Example 1: fch_mo_copy a.fch b.fch      (Alpha MOs in a.fch -> Alpha MOs in b.fch)'
+  write(iout,'(A)')   ' Example 2: fch_mo_copy a.fch b.fch -aa  (eqv. to Example1)'
+  write(iout,'(A)')   ' Example 3: fch_mo_copy a.fch b.fch -ab  (Alpha MOs in a.fch -> Beta  MOs in b.fch)'
+  write(iout,'(A)')   ' Example 4: fch_mo_copy a.fch b.fch -ba  (Beta  MOs in a.fch -> Alpha MOs in b.fch)'
+  write(iout,'(A)')   ' Example 5: fch_mo_copy a.fch b.fch -bb  (Beta  MOs in a.fch -> Beta  MOs in b.fch)'
+  write(iout,'(A,/)')   ' Example 6: fch_mo_copy a.fch b.fch 5 10 (copy 5-10 Alpha MOs in a.fch -> b.fch)'
+  stop
+ end if
+
+ call getarg(1, fchname1)
+ call getarg(2, fchname2)
+
+ if(i == 3) then
+  call getarg(3,ab)
+  if(ab(1:1) /= '-') then
+   write(iout,'(A)') "ERROR in subroutine fch_mo_copy: the 3rd argument must be '-aa'/'-ab'/'-ba'/'-bb'."
+   stop
+  end if
+
+ else ! i == 4
+  call getarg(3,str)
+  read(str,*) idx1
+  call getarg(4,str)
+  read(str,*) idx2
+
+  if(idx1<1 .or. idx2<1 .or. idx1>idx2) then
+   write(iout,'(A)') 'ERROR in subroutine fch_mo_copy: invalid indices idx1 and/or idx2.'
+   write(iout,'(2(A,I0))') 'idx1=', idx1, ', idx2=', idx2
+   stop
+  end if
+ end if
+
+ call fch_mo_copy(fchname1, fchname2, ab, idx1, idx2)
+ stop
+end program main
+
+! copy Alpha/Beta MOs from a .fch file into Alpha/Beta MOs of another .fch file
+subroutine fch_mo_copy(fchname1, fchname2, ab, idx1, idx2)
+ implicit none
+ integer :: nbf, nif, nbf2, nif2
+ integer, intent(in) :: idx1, idx2
+ integer, parameter :: iout = 6
+ real(kind=8), allocatable :: coeff1(:,:), coeff2(:,:)
+ character(len=3), intent(in) :: ab
+ character(len=240), intent(in) :: fchname1, fchname2
+
+ call read_nbf_and_nif_from_fch(fchname1, nbf, nif)
+ call read_nbf_and_nif_from_fch(fchname2, nbf2, nif2)
+
+ if(nbf/=nbf2 .or. nif/=nif2) then
+  write(iout,'(A)') 'ERROR in subroutine fch_mo_copy: nbf and/or nif not equal in two files.'
+  write(iout,'(A)') 'File1: '//TRIM(fchname1)//', File2: '//TRIM(fchname2)
+  stop
+ end if
+
+ allocate(coeff1(nbf,nif), source=0.0d0)
+ allocate(coeff2(nbf,nif), source=0.0d0)
+ call read_mo_from_fch(fchname1, nbf, nif, ab(2:2), coeff1)
+ call read_mo_from_fch(fchname2, nbf, nif, ab(3:3), coeff2)
+
+ if(idx1==0 .and. idx2==0) then
+  coeff2 = coeff1
+ else ! 0 < idx1 <= idx2
+  coeff2(:,idx1:idx2) = coeff1(:,idx1:idx2)
+ end if
+
+ call write_mo_into_fch(fchname2, nbf, nif, ab(3:3), coeff2)
+ deallocate(coeff1, coeff2)
+ return
+end subroutine fch_mo_copy
+
