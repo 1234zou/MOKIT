c5266687460161411fed08e577ca9ed2ae270b01
hebrewsnabla <srwang20@fudan.edu.cn>
2022-07-26 22:54:46 +0800

    update test

--- test/test_py2fch.py
+++ test/test_py2fch.py
@@ -1,6 +1,11 @@
 from pyscf import gto
 from py2fch_direct import mol2fch
+from py2fch import py2fch
 
 mol = gto.M(atom='O 0.0 0.0 0.1; H 0.0 0.0 1.0', basis='cc-pvtz', charge=-1).build()
+mf = mol.RHF().run()
 
-mol2fch(mol)
+#S = mol.intor('int1e_ovlp')
+#print(S)
+mol2fch(mol, 'test.fch')
+py2fch('test.fch', mol.nao, mol.nao, mf.mo_coeff, 'a', mf.mo_energy, 0)
