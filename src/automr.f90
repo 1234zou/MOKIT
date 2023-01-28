247e14ffdfa68a7b1a9f5e825923be09c991dcf5
jxzou <njumath@sina.cn>
2023-01-28 11:50:24 +0800

    add read_iframe_from_xyz

--- src/automr.f90
+++ src/automr.f90
@@ -27,7 +27,7 @@ program main
 
  select case(TRIM(fname))
  case('-v', '-V', '--version')
-  write(6,'(A)') 'AutoMR 1.2.5 :: MOKIT, release date: 2023-Jan-26'
+  write(6,'(A)') 'AutoMR 1.2.5 :: MOKIT, release date: 2023-Jan-28'
   stop
  case('-h','-help','--help')
   write(6,'(/,A)') "Usage: automr [gjfname] >& [outname]"
