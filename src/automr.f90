2f2c9ad26c965a864e3d38acbce4468ac32983e2
jxzou <njumath@sina.cn>
2025-10-14 09:36:59 +0800

    update date and build number

--- src/automr.f90
+++ src/automr.f90
@@ -26,7 +26,7 @@ program main
 
  select case(TRIM(fname))
  case('-v', '-V', '--version')
-  write(6,'(A)') 'AutoMR 1.2.7rc12 :: MOKIT, release date: 2025-Oct-12'
+  write(6,'(A)') 'AutoMR 1.2.7rc12 :: MOKIT, release date: 2025-Oct-13'
   stop
  case('-h','-help','--help')
   write(6,'(/,A)') 'Usage: automr [gjfname] > [outname]'
--- src/mr_keyword.f90
+++ src/mr_keyword.f90
@@ -308,7 +308,7 @@ subroutine read_program_path()
  write(6,'(A)') '------ Output of AutoMR of MOKIT(Molecular Orbital Kit) ------'
  write(6,'(A)') '       GitLab page: https://gitlab.com/jxzou/mokit'
  write(6,'(A)') '     Documentation: https://jeanwsr.gitlab.io/mokit-doc-mdbook'
- write(6,'(A)') '           Version: 1.2.7rc12 (2025-Oct-12)'
+ write(6,'(A)') '           Version: 1.2.7rc12 (2025-Oct-13)'
  write(6,'(A)') '       How to cite: see README.md or $MOKIT_ROOT/doc/'
 
  hostname = ' '
